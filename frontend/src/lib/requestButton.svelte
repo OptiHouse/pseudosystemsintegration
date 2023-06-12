1f10187dd365ef7bc867702208cddcb61eaacbe0
Filip <filiptarajko@gmail.com>
2023-06-12 19:23:37 +0200

    Add sortable table for loaded year+crime

--- frontend/src/lib/requestButton.svelte
+++ frontend/src/lib/requestButton.svelte
@@ -2,7 +2,9 @@
 	let loading = false;
 	let justLoaded = false;
 	export let awaitedFunction: () => Promise<any>;
-	export let myData: any;
+	export let responseData: any;
+	export let text: string;
+	export let to_be_sorted = true;
 
 	async function loadData() {
 		loading = true;
@@ -13,7 +15,22 @@
 			justLoaded = false;
 		}, 3000);
 		console.log(response);
-		myData = response.data;
+		if (to_be_sorted) {
+			for (let i = 0; i < response.data.length; i++) {
+				// console.log(response.data[i]?.statistics[0]);
+				response.data = response.data.filter(
+					(elem) => elem.name != 'United States' && elem.name != 'State'
+				);
+				response.data[i].statistics[0]?.population.sort((a, b) => {
+					if (a.name > b.name) {
+						return 1;
+					} else {
+						return -1;
+					}
+				});
+			}
+		}
+		responseData = response.data;
 		return {
 			response
 		};
@@ -22,19 +39,34 @@
 	import { ProgressRadial } from '@skeletonlabs/skeleton';
 </script>
 
-<button class="btn variant-filled" disabled={loading} on:click={loadData}>
-	{#if !loading}
-		load data
-	{:else}
-		loading...
-	{/if}
-</button>
-<!-- <div style="position: relative; right: 200px;"> -->
-{#if loading || justLoaded}
-	<ProgressRadial
-		value={justLoaded ? 100 : undefined}
-		stroke={10}
-		track={justLoaded ? 'stroke-success-500/30' : 'stroke-primary-500/30'}
-		meter={justLoaded ? 'stroke-success-500' : 'stroke-primary-500'}
-	/>
-{/if}
+<link
+	rel="stylesheet"
+	href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+/>
+
+<div style="display: flex; flex-direction: row;">
+	<button
+		class="btn variant-filled"
+		style="width: 200px; margin: 4px;"
+		disabled={loading}
+		on:click={loadData}
+	>
+		{#if !loading}
+			{text}
+		{:else}
+			<!-- loading... -->
+			Loading <i style="margin-left: 4px;" class="fa fa-spinner fa-spin" />
+		{/if}
+	</button>
+	<!-- <div style="position: relative; right: 200px;"> -->
+	<!-- <div style="position:relative; right: 200px;">
+		{#if loading || justLoaded}
+			<ProgressRadial
+				value={justLoaded ? 100 : undefined}
+				stroke={10}
+				track={justLoaded ? 'stroke-success-500/30' : 'stroke-primary-500/30'}
+				meter={justLoaded ? 'stroke-success-500' : 'stroke-primary-500'}
+			/>
+		{/if}
+	</div> -->
+</div>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,33 +1,144 @@
 <script lang="ts">
 	import axios from 'axios';
 	import RequestButton from '$lib/requestButton.svelte';
-	let myData = '';
+	let dataLoadingResponse = '';
+	let currently_analyzed_data: any = [];
+	let calculating = false;
+	let sorted_by = '?';
+
+	function reorder_by_rate() {
+		calculating = true;
+		console.log(currently_analyzed_data);
+		if (sorted_by != 'rate_asc') {
+			currently_analyzed_data = currently_analyzed_data.sort((a, b) => {
+				return a?.statistics[0]?.crimes[0]?.rate - 0 > b?.statistics[0]?.crimes[0]?.rate - 0
+					? 1
+					: -1;
+			});
+			sorted_by = 'rate_asc';
+		} else {
+			currently_analyzed_data = currently_analyzed_data.sort((a, b) => {
+				return a?.statistics[0]?.crimes[0]?.rate - 0 < b?.statistics[0]?.crimes[0]?.rate - 0
+					? 1
+					: -1;
+			});
+			sorted_by = 'rate_desc';
+		}
+		calculating = false;
+	}
+
+	function reorder_by_state() {
+		calculating = true;
+		console.log(currently_analyzed_data);
+		if (sorted_by != 'state_asc') {
+			currently_analyzed_data = currently_analyzed_data.sort((a, b) => {
+				return a?.name > b?.name ? 1 : -1;
+			});
+			sorted_by = 'state_asc';
+		} else {
+			currently_analyzed_data = currently_analyzed_data.sort((a, b) => {
+				return a?.name < b?.name ? 1 : -1;
+			});
+			sorted_by = 'state_desc';
+		}
+		calculating = false;
+	}
+
+	function reorder_by_race(race) {
+		calculating = true;
+		console.log(currently_analyzed_data);
+		if (sorted_by != `${race}_asc`) {
+			currently_analyzed_data = currently_analyzed_data.sort((a, b) => {
+				// currently_analyzed_data[0]?.statistics[0]?.population[i]?.name
+				return a?.statistics?.[0]?.population[race]?.population - 0 >
+					b?.statistics?.[0]?.population[race]?.population - 0
+					? 1
+					: -1;
+				// return a?.name > b?.name ? 1 : -1;
+			});
+			sorted_by = `${race}_asc`;
+		} else {
+			currently_analyzed_data = currently_analyzed_data.sort((a, b) => {
+				return a?.statistics?.[0]?.population[race]?.population - 0 <
+					b?.statistics?.[0]?.population[race]?.population - 0
+					? 1
+					: -1;
+			});
+			sorted_by = `${race}_desc`;
+		}
+		calculating = false;
+	}
 </script>
 
-<div class="container h-full mx-auto flex justify-center items-center">
+<div class="container h-full mx-auto flex justify-start items-center flex-col p-16">
 	<RequestButton
-		bind:myData
+		text="load data to DB"
+		to_be_sorted={false}
+		bind:responseData={dataLoadingResponse}
 		awaitedFunction={() => {
 			return axios.post('http://localhost:8080/data/loadData', {
-				// return axios.get('http://localhost:8069/posts', {
 				headers: {
-					'Content-Type': 'application/json'
+					'Content-Type': 'application/json',
+					'Access-Control-Allow-Origin': '*'
+				}
+			});
+		}}
+	/>
+	<RequestButton
+		text="get data from DB"
+		bind:responseData={currently_analyzed_data}
+		awaitedFunction={() => {
+			return axios.get('http://localhost:8080/graphs?years=2013&crimes=Murder', {
+				headers: {
+					'Content-Type': 'application/json',
+					'Access-Control-Allow-Origin': '*'
 				}
 			});
 		}}
 	/>
 
+	{#if currently_analyzed_data.length}
+		<div class="table-container" style="margin-top: 8px;">
+			<table class="table table-hover">
+				<thead>
+					<tr>
+						<th><button disabled={calculating} on:click={reorder_by_state}>Name</button></th>
+						<th><button disabled={calculating} on:click={reorder_by_rate}>Rate</button></th>
+						{#if currently_analyzed_data.length > 1}
+							{#each { length: currently_analyzed_data[0]?.statistics[0]?.population?.length } as _, i}
+								<th>
+									<button
+										disabled={calculating}
+										on:click={() => {
+											console.log(i);
+											reorder_by_race(i);
+										}}>{currently_analyzed_data[0]?.statistics[0]?.population[i]?.name}</button
+									>
+								</th>
+							{/each}
+						{/if}
+					</tr>
+				</thead>
+				<tbody>
+					{#each currently_analyzed_data as row, i}
+						{#if row.name != 'State' && row.name != 'United States'}
+							<tr>
+								<td>{row.name}</td>
+								<td>{row.statistics[0].crimes[0].rate}</td>
+								{#each { length: currently_analyzed_data[0]?.statistics[0]?.population?.length } as _, i}
+									<td>{Math.round(row?.statistics[0]?.population[i]?.population * 100) / 100}</td>
+								{/each}
+							</tr>
+						{/if}
+					{/each}
+				</tbody>
+			</table>
+		</div>
+	{/if}
+
 	<div>
-		{#if myData}
-			myData exists :D
-			<!-- {#each myData as post}
-				<div class="card">
-					<div class="card-body">
-						<h5 class="card-title"><b>{post.title}</b></h5>
-						<p class="card-text">{post.text}</p>
-					</div>
-				</div>
-			{/each} -->
+		{#if dataLoadingResponse}
+			dataLoadingResponse
 		{/if}
 	</div>
 </div>
